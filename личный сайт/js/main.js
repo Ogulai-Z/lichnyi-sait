f35e11cfa2f74186ceb1d1b2e58d728c8d323b4f
Ogulai-Z <53644263+Ogulai-Z@users.noreply.github.com>
2021-01-10 09:52:23 +0300

    Add files via upload

---
+++ личный сайт/js/main.js
@@ -0,0 +1,9 @@
+$(window).on('load', function(){   //при загрузке странице заработает функция видео
+    
+//vide js-video bcg
+
+$('#header').vide('./video/cover', {     //выбираем по айди и подключим 
+    bgColor: '#642519'
+});
+
+});
